1c139f833c6a218ffd7c6366cb90503031bbc932
YonatanSetiawan <yonatansetiawan76@gmail.com>
2023-09-13 16:34:41 +0700

    update

---
+++ js/script.js
@@ -0,0 +1,44 @@
+// navbar
+let navbar_button = document.querySelector('.navbar-button');
+let navLinks = document.querySelector('.nav-links');
+
+navbar_button.addEventListener('click', () => {
+    navLinks.classList.toggle('nav-active');
+});
+
+// faq
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        let panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+
+
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+
+//hero
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("hero-content");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}

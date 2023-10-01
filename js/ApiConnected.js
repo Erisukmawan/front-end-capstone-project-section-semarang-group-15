443c8cbde6663f60954e7a5413038bf9c52d1cfd
Eri Sukmawan <erisukmawan03@gmail.com>
2023-10-01 19:42:47 +0700

    update

--- js/ApiConnected.js
+++ js/ApiConnected.js
@@ -1,26 +1,33 @@
 // Api Get Testimoni
 function getTestimoni() {
-    fetch('https://languago.up.railway.app/api/v1/testimoni')
-        .then(response => response.json()).then(result => { renderTestimoni(result.data) })
+    fetch('https://languago.up.railway.app/api/v1/testimoni', {
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                },
+        })
+        .then(response => response.json()).then(data => {  
+            let testiContainer = document.getElementById("testimoniContainer");
+            for (var value of data) {
+                testiContainer.innerHTML += `<div class="testimoni-card" >
+                    <div class="testimoni-img">
+                        <img src="images/${(value.foto)}" alt="Person 1">
+                    </div>
+                    <div class="testimoni-author">
+                        <p>${(value.nama)}</p>
+                    </div>
+                    <div class="testimoni-program">
+                        <p>${(value.nama_program)}</p>
+                    </div>
+                    <div class="testimoni-text">
+                        <p>“${(value.pesan_testimoni)}”</p>
+                    </div>
+                </div>`;
+            }
+        })
         .catch(error => console.log('Error Fatching Data :', error));
 }
 function renderTestimoni(data) {
-    let testiContainer = document.getElementById("testimoniContainer");
-    for (var i = 0; i < data.length; i++) {
-        testiContainer.innerHTML += `<div class="testimoni-card" >
-                    <div class="testimoni-img">
-                        <img src="images/${(data[i].foto)}" alt="Person 1">
-                    </div>
-                    <div class="testimoni-author">
-                        <p>${(data[i].nama)}</p>
-                    </div>
-                    <div class="testimoni-program">
-                        <p>${(data[i].nama_program)}</p>
-                    </div>
-                    <div class="testimoni-text">
-                        <p>“${(data[i].pesan_testimoni)}”</p>
-                    </div>
-                </div>`;
-    }
+    console.log(data)
+    
 }
 getTestimoni();

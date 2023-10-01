5f4c42f8e4a9535343658f26839ef003ebf02a18
Eri Sukmawan <erisukmawan03@gmail.com>
2023-10-01 22:55:27 +0700

    add post

--- js/ApiConnected.js
+++ js/ApiConnected.js
@@ -30,3 +30,33 @@ function renderTestimoni(data) {
     console.log(data)
 }
 getTestimoni();
+
+// Post Kontak Kami
+const kontakForm = document.querySelector('#kontak_kami');
+
+kontakForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    let nama = document.querySelector("#NamaKontak").value;
+    let email = document.querySelector("#EmailKontak").value;
+    let pesan = document.querySelector("#PesanKontak").value;
+
+    const url = "https://languago.up.railway.app/api/v1/kontak-kami"
+    fetch(url, {
+        method: "POST",
+        body: JSON.stringify({
+            nama: nama,
+            email: email,
+            isi_pesan: pesan
+        }),
+        headers: {
+            'Content-type': 'application/json',
+        },
+    }).then(response => response.json())
+        .then(data => alert("Berhasil Terkirim"))
+        .catch(error => alert(error))
+
+    document.querySelector("#NamaKontak").value = "";
+    document.querySelector("#EmailKontak").value = "";
+    document.querySelector("#PesanKontak").value = "";
+});
